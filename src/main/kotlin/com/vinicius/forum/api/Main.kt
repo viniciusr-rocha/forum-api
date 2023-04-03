03126f1e3d8a35f49b083a6ddcb3d840def66bdb
Vinicius Rocha <3rd.vinicius.rocha@amedigital.com>
2023-04-03 07:55:18 -0300

    feat: Criado projeto base

---
+++ src/main/kotlin/com/vinicius/forum/api/Main.kt
@@ -0,0 +1,15 @@
+package com.vinicius.forum.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>) {
+    runApplication<Main>(*args)
+}
